aafb884bd508c65fb70d41f5e4e0eb445879704f
Rinaldo23 <rinaldobadigar98@gmail.com>
2022-07-14 00:12:28 +0530

    font color button implemented

--- Browser/Excel_Clone/script.js
+++ Browser/Excel_Clone/script.js
@@ -6,40 +6,38 @@ let addressBar = document.querySelector(".address_bar");
 let rows = 100;
 let cols = 26;
 
-for( let i = 0 ; i < rows ; i++){
-    let colCell = document.createElement("div");
-    colCell.setAttribute("class", "col-cell");
-    colCell.innerText = i + 1;
-    leftColCont.appendChild(colCell);
+for (let i = 0; i < rows; i++) {
+  let colCell = document.createElement("div");
+  colCell.setAttribute("class", "col-cell");
+  colCell.innerText = i + 1;
+  leftColCont.appendChild(colCell);
 }
 
-for( let i = 0 ; i < cols ; i++){
-    let rowCell = document.createElement("div");
-    rowCell.setAttribute("class", "row-cell");
-    rowCell.innerText = String.fromCharCode(65 + i);
-    topRowCont.appendChild(rowCell);
+for (let i = 0; i < cols; i++) {
+  let rowCell = document.createElement("div");
+  rowCell.setAttribute("class", "row-cell");
+  rowCell.innerText = String.fromCharCode(65 + i);
+  topRowCont.appendChild(rowCell);
 }
 
-
-for( let i = 0 ; i < rows ; i++){
-    let rowCont = document.createElement("div");
-    rowCont.setAttribute("class", "row-cont");
-    for( let j = 0 ; j < cols ; j++){
-        let gridCell = document.createElement("div");
-        gridCell.setAttribute("class", "grid-cell");
-        gridCell.setAttribute("contenteditable", "true");
-        rowCont.appendChild(gridCell);
-        getCellAddress(gridCell, i, j);
-    }
-    cellGridCont.appendChild(rowCont);
+for (let i = 0; i < rows; i++) {
+  let rowCont = document.createElement("div");
+  rowCont.setAttribute("class", "row-cont");
+  for (let j = 0; j < cols; j++) {
+    let gridCell = document.createElement("div");
+    gridCell.setAttribute("class", "grid-cell");
+    gridCell.setAttribute("contenteditable", "true");
+    rowCont.appendChild(gridCell);
+    getCellAddress(gridCell, i, j);
+  }
+  cellGridCont.appendChild(rowCont);
 }
-
 
 // addressBar Implementation
-function getCellAddress(cell, row, col){
-    cell.addEventListener("click", function(e){
-        rowID = row + 1;
-        colID = String.fromCharCode(65 + col);
-        addressBar.value = `${colID}${rowID}`;
-    })
+function getCellAddress(cell, row, col) {
+  cell.addEventListener("click", function (e) {
+    rowID = row + 1;
+    colID = String.fromCharCode(65 + col);
+    addressBar.value = `${colID}${rowID}`;
+  });
 }
